e2c8116621dc50ffee64fe80373419efd7c1e50f
ThiagobCoelho <tbarroscoelho@gmail.com>
2023-07-04 21:36:39 -0300

    Seção de Habilidades finalizada

--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import js from "../../assets/skills/js.svg"
+import css from "../../assets/skills/css.svg"
+import html from "../../assets/skills/html.svg"
+import react from "../../assets/skills/react.svg"
+import "./Skills.scss"
 
 const Skills = () => {
   return (
-    <div id="Skills">Skills</div>
+    <div className='Skills' id="Skills">
+      <h1>Habilidades</h1>
+      <p>Conheça um pouco das minhas habilidades e conhecimentos.</p>
+      <div className="Skills__images">
+        <img src={js} alt="" />
+        <img src={css} alt="" />
+        <img src={html} alt="" />
+        <img src={react} alt="" />
+      </div>
+    </div>
   )
 }
 
